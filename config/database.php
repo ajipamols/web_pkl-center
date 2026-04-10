1c9a8dbe1aaad2cdaf9b3d52d3ca807d181e8b74
ajipamols <prada7643@gmail.com>
2026-04-11 05:24:59 +0700

    coommit nginx docker compose

--- config/database.php
+++ config/database.php
@@ -5,9 +5,9 @@
  */
 
 // Konfigurasi Database
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_HOST', 'kelompok9');
+define('DB_USER', 'admin');
+define('DB_PASS', 'AdminPkl@2026!');
 define('DB_NAME', 'pkl_center');
 
 // Koneksi ke Database
